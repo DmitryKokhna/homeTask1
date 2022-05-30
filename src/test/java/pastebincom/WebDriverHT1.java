1cf97271ecf5594086ea006522f59e4d7b0d7e88
Dmitry Kokhna from consol <dmitry.kokhna@gmail.com>
2022-05-30 21:42:31 +0300

    done

--- src/test/java/pastebincom/WebDriverHT1.java
+++ src/test/java/pastebincom/WebDriverHT1.java
@@ -7,23 +7,48 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverHT1 {
+
+/**
+переменные
+ */
+    public static String textForPuttiong = "Hello from WedDiver. I can Win";
+    public static String nameTitle = "helloweb";
+
+
     public static void main(String[] args) {
 
         WebDriver driverCh = new ChromeDriver();
-//"//a[contains(text(),'paste')]")  ("postform-text") & By.name("PostForm[text])");
-
-        //  searchPlusPasteButton.click();
+// открываем
         driverCh.get("https://pastebin.com/");
+// поиск поля для ввода
         WebElement textField = driverCh.findElement(By.xpath("//*[@id='postform-text' and @name='PostForm[text]']"));
+// на влякий случай очищаем
         textField.clear();
+// вводим текст
+        textField.sendKeys(textForPuttiong);
 
-        textField.sendKeys("I can Win");
+// имем выпадающий список
+        WebElement findDropDown = driverCh.findElement(By.id("select2-postform-expiration-container"));
+// кликаем на авпадающий список
+        findDropDown.click();
 
-              WebElement findDropDown = driverCh.findElement(By.id("select2-postform-expiration-container"));
-              findDropDown.click();
+// ищем 10 мин. как не черех абсолютный путь хз
+        WebElement clickOn10Min = driverCh.findElement(By.xpath("/html/body/span[2]/span/span[2]/ul/li[3]"));
+// кликаем на 10 мин
+        clickOn10Min.click();
 
-WebElement clickOn10Min= driverCh.findElement(By.xpath("/html/body/span[2]/span/span[2]/ul/li[3]"));
-clickOn10Min.click();
+// ищем поле для Имени
+        WebElement pasteNameTitle = driverCh.findElement(By.id("postform-name"));
+// пишем в поле заданное имя
+        pasteNameTitle.sendKeys(nameTitle);
 
+// ищем кнопию Отправить
+        WebElement buttonSubmit = driverCh.findElement(By.xpath("//*[@type='submit']"));
+// кликаем кнопию отправить
+        buttonSubmit.click();
+
+// для красоты
+        System.out.println();
+        System.out.println("The task has been done");
     }
 }
